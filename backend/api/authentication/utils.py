d4b7efe7eb7482c7d0e68326b16c457db8a4e4e8
Ricardo Leon <ricardo.leon@telefonica.com>
2018-07-31 16:33:51 -0400

    Delete print statements

--- backend/api/authentication/utils.py
+++ backend/api/authentication/utils.py
@@ -10,11 +10,8 @@ def is_valid_new_password(user, passwd):
     # Get user previous passwords
     last_passwd = PasswordHistory.objects.filter(user=user) \
                         .order_by('-passwd_date').values_list('password')[:10]
-    print(last_passwd)
     # Check if password is in array
     for password in last_passwd:
-        print(password)
-        print(password[0])
         if check_password(passwd, password[0]):
             return False
     return True
