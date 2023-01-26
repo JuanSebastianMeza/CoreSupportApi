eb1e6721a6c17daa0e4c9a817743a7f64b88839b
Julio Barrios <julio.barrios@telefonica.com>
2023-01-25 23:38:24 -0400

    jupyter lab

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -1,12 +1,13 @@
 """
 Jupyter configs
 """
-c.NotebookApp.allow_origin = '*' # pylint: disable=undefined-variable
-c.NotebookApp.ip = '0.0.0.0' # pylint: disable=undefined-variable
+c.ServerApp.allow_origin = '*' # pylint: disable=undefined-variable
+c.ServerApp.ip = '0.0.0.0' # pylint: disable=undefined-variable
 
 c.NotebookApp.notebook_dir = '/backend/notebooks' # pylint: disable=undefined-variable
 c.NotebookApp.open_browser = False # pylint: disable=undefined-variable
-c.NotebookApp.allow_root = True # pylint: disable=undefined-variable
-c.NotebookApp.port = 8888 # pylint: disable=undefined-variable
-c.NotebookApp.token = u'' # pylint: disable=undefined-variable
-c.NotebookApp.password = 'sha1:29a62bd8ccb0:c575f955f7892be90b5e05514525c60f5dc5135a' # pylint: disable=undefined-variable
+c.ServerApp.allow_root = True # pylint: disable=undefined-variable
+c.ServerApp.port = 8888 # pylint: disable=undefined-variable
+c.ServerApp.token = u'' # pylint: disable=undefined-variable
+c.ServerApp.password = 'sha1:82de3bd8aa97:f27b2f5aa414298a84a0a42c36a596edff7a99c0' # pylint: disable=undefined-variable
+c.NotebookApp.default_url = '/lab'
