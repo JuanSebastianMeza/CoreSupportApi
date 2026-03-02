cbcd4e82496560f454e1502d016c3a7611b4ece4
JUAN SEBASTIAN MEZA <gjuans.pd@gmail.com>
2026-03-02 08:41:59 -0400

    Improve home commponent

--- frontend/src/app/private-views/home/home.component.ts
+++ frontend/src/app/private-views/home/home.component.ts
@@ -7,7 +7,6 @@ import { ConstService } from '../../services/utils/const.service';
 // Import global class
 import { Globals } from '../../globals';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,24 +14,35 @@ import { Globals } from '../../globals';
 })
 export class HomeComponent implements OnInit {
 
+  // List of available tools
+  tools = [
+    {
+      name: 'Subscriber Data Collector',
+      description: 'Consulta datos detallados de suscriptores en la red móvil (LTE/UMTS/GSM) usando su número MSISDN.',
+      icon: '📡',
+      route: '/subscriber-data-collector',
+      color: 'blue'
+    }
+    // Future tools can be added here easily
+  ];
+
   constructor(
     // Inject services
     private http: HttpRequestsService,
     private auth: AuthService,
     private constService: ConstService,
     // Inject global class
-    private globals: Globals,
+    public globals: Globals,
 
   ) { }
 
   ngOnInit() {
-    
     if (this.constService.ambiente) {
       // Si es ambiente de producción, incluir posteo de acceso a la herramienta.
       this.http.postAppAuditInfo(
         this.globals.appHomeId,
         this.auth.getUserId()
-      ); 
+      );
     }
   }
 
